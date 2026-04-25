1aa5999fa2bae33aa44e66286e5ef35f6af1695c
FLYCER-6c06-4dca-954b <ann.vandalize@gmail.com>
2026-04-26 06:11:59 +0700

    Implement challenge API with security layers

--- api/robots.js
+++
@@ -1,7 +0,0 @@
-export default function handler(req, res) {
-  res.setHeader("Content-Type", "text/plain; charset=utf-8");
-  res.setHeader("Cache-Control", "public, max-age=86400");
-  return res.status(200).send(
-    "User-agent: *\nDisallow: /\n\nUser-agent: Googlebot\nDisallow: /\n\nUser-agent: Bingbot\nDisallow: /\n"
-  );
-}
