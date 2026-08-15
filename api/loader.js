4b4dd0303438d7fcccce8741e34d5e56eea38add
FLYCER-6c06-4dca-954b <ann.vandalize@gmail.com>
2026-08-15 21:50:55 +0700

    Replace Testing loader with new loader for RoBeatsV2.04

--- api/loader.js
+++ api/loader.js
@@ -10,10 +10,10 @@ export const LOADERS = {
 
   // ── Tambah loader baru di bawah ini ───────────────────────────────────
 
-  "v2/Testing": {
-    url:    "https://raw.githubusercontent.com/Lyfe-e40d0ba8-d728-4fcf-9e39/Main/refs/heads/main/Test",
+  "v2/af540ea0725a": {
+    url:    "https://raw.githubusercontent.com/Iamnewcodethis2/RobeatsRevamp/main/233/RoBeatsV2.04/Main.lua",
     active: true,
-    note:   "Script 2",
+    note:   "Script milik Maxwell - RobeatsRevamp",
   },
 
   "v3/premium": {
