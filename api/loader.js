c9f2a10c55fd326c6774f18b7326d7f1a2f2e576
FLYCER-6c06-4dca-954b <ann.vandalize@gmail.com>
2026-09-26 12:05:25 +0700

    Update loader.js

--- api/loader.js
+++ api/loader.js
@@ -2,28 +2,28 @@
 
 export const LOADERS = {
 
-  // ── Format key: "vX/namaUnik" ──────────────────────────────────────────
-  // key   : digunakan di URL → /loaders/v2/ae3a52604adf
-  // url   : raw URL script Lua yang akan di-load executor
-  // active: false = return 403, true = aktif
-  // note  : deskripsi (tidak dikirim ke client)
+    // Format key: "vX/namaUnik"
+    // key   : digunakan di URL → /loaders/v2/ae3a52604adf
+    // url   : raw URL script Lua yang akan di-load executor
+    // active: false = return 403, true = aktif
+    // note  : deskripsi (tidak dikirim ke client)
 
-  "v2/ae3a52604adf": {
-    url:    "https://raw.githubusercontent.com/Lyfe-e40d0ba8-d728-4fcf-9e39/supported-b045ccea-1f23-4201-866b-0b3c3381cdba/main/CarControllerUniversal",
-    active: true,
-    note:   "Car Controller for Mobile Universal",
-  },
+    "v2/ae3a52604adf": {
+        url: "https://raw.githubusercontent.com/Lyfe-e40d0ba8-d728-4fcf-9e39/supported-b045ccea-1f23-4201-866b-0b3c3381cdba/main/CarControllerUniversal",
+        active: true,
+        note: "Car Controller for Mobile Universal",
+    },
 
-  "v2/af540ea0725a": {
-    url:    "https://raw.githubusercontent.com/Iamnewcodethis2/RobeatsRevamp/main/233/RoBeatsV2.04/Main.lua",
-    active: true,
-    note:   "Script milik Maxwell - RobeatsRevamp",
-  },
+    "v2/af540ea0725a": {
+        url: "https://raw.githubusercontent.com/Iamnewcodethis2/RobeatsRevamp/main/233/RoBeatsV2.04/Main.lua",
+        active: true,
+        note: "Script milik Maxwell - RobeatsRevamp",
+    },
 
-  "v2/premium": {
-    url:    "https://raw.githubusercontent.com/Kyoukara/Loader/refs/heads/main/soms.lua",
-    active: true,
-    note:   "SOMS)",
-  },
+    "v2/premium": {
+        url: "https://raw.githubusercontent.com/Kyoukara/Loader/refs/heads/main/soms.lua",
+        active: true,
+        note: "SOMS)",
+    },
 
 };
